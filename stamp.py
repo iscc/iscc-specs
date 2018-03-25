8eb2385995f5f50045af3f976262f82c80fe3f39
Titusz Pan <titusz.pan@gmail.com>
2018-03-25 16:49:37 +0200

    iscc stamp site and spec separately

--- stamp.py
+++ stamp.py
@@ -2,9 +2,11 @@
 """Generate an ISCC-Text for this specification"""
 import os
 import iscc
+from subprocess import call
 
 
 def get_content(mode='text'):
+    call(['mkdocs', 'build'])
 
     content = '' if mode == 'text' else b''
     if mode == 'text':
@@ -24,22 +26,37 @@ def get_content(mode='text'):
     return content
 
 
-def main():
-    title = "ISCC - Content Identifiers"
-    extra = "Version 0.9.7"
-    text = get_content('text')
-    data = get_content('data')
+def spec_iscc():
+    title = "ISCC - Specification"
+    extra = "v0.9.7"
+    text = open('docs/specification.md', encoding='utf-8').read()
+    data = open('docs/specification.md', 'rb').read()
     mid, title, extra = iscc.meta_id(title, extra)
     cidt = iscc.content_id_text(text)
     did = iscc.data_id(data)
     iid, hash_ = iscc.instance_id(data)
-    code = ''.join((mid, cidt, did, iid))
+    code = '-'.join((mid, cidt, did, iid))
+    print('SPEC:')
+    print('TITLE:', title, extra)
     print('ISCC:', code)
-    print('Title:', title)
-    if extra:
-        print('Extra:', extra)
-    print('Hash:', hash_)
+    print('IIDF:', hash_)
+
+
+def site_iscc():
+    title = "ISCC - Content Identifiers"
+    text = get_content('text')
+    data = get_content('data')
+    mid, title, extra = iscc.meta_id(title)
+    cidt = iscc.content_id_text(text)
+    did = iscc.data_id(data)
+    iid, hash_ = iscc.instance_id(data)
+    code = '-'.join((mid, cidt, did, iid))
+    print('SITE:')
+    print('TITLE:', title, extra)
+    print('ISCC:', code)
+    print('IIDF:', hash_)
 
 
 if __name__ == '__main__':
-    main()
+    spec_iscc()
+    site_iscc()
