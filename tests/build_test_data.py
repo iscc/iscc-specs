ade9e896e095622054a5c94698a47e80ce9eb218
Titusz Pan <titusz.pan@gmail.com>
2019-05-22 17:56:39 +0200

    Build testdata from json inputs to remove YAML dependency

--- tests/build_test_data.py
+++ tests/build_test_data.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-import yaml
 import json
 import iscc
 
 
 def main():
-    data = yaml.load(open('test_inputs.yaml', 'rb'))
+    data = json.load(open('test_inputs.json', 'r', encoding='utf-8'))
+
     for funcname, tests in data.items():
         for testname, testdata in tests.items():
             func = getattr(iscc, funcname)
@@ -13,7 +13,6 @@ def main():
             result = func(*args)
             if funcname in ['data_chunks']:
                 result = ['hex:' + data.hex() for data in result]
-                print(len(result))
             testdata['outputs'] = result
     with open('test_data.json', 'w', encoding='utf-8') as outf:
         json.dump(data, outf, indent=2, sort_keys=True, ensure_ascii=False)
