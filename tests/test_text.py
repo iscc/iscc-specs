bbf5f52c13bd86500b8f21519d3265f9ff7c7ddc
Titusz Pan <titusz.pan@gmail.com>
2020-09-16 09:54:13 +0200

    Update dependencies.

--- tests/test_text.py
+++ tests/test_text.py
@@ -6,6 +6,4 @@ from fauxfactory.factories.strings import gen_utf8
 def test_text_chunks():
     txt = gen_utf8(1024 * 100)
     chunks = list(text_chunks(txt, avg_size=1024))
-    assert ''.join(chunks) == txt
-
-
+    assert "".join(chunks) == txt
