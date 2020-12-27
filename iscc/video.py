4247c77f3c2072bc92e33a89a33b20e9f666f021
Titusz Pan <titusz.pan@gmail.com>
2020-12-27 19:48:36 +0100

    Add incremental mp7 signature generator.

--- iscc/video.py
+++ iscc/video.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import subprocess
+from loguru import logger as log
 import os
 import sys
-import subprocess
+from subprocess import Popen, PIPE, DEVNULL
 from os.path import basename, dirname
+from secrets import token_hex
+from typing import Generator
 import imageio_ffmpeg
 from statistics import mode
 from iscc.utils import cd
@@ -11,9 +15,8 @@ from iscc.utils import cd
 FFMPEG = imageio_ffmpeg.get_ffmpeg_exe()
 
 
-def compute_signature(file_path: str) -> bytes:
+def compute_signature(file_path: str, crop=None) -> bytes:
     """Computes MP7 Video Signature"""
-    crop = detect_crop(file_path)
     sigfile = basename(file_path) + ".bin"
     folder = dirname(file_path)
     if crop:
@@ -29,12 +32,37 @@ def compute_signature(file_path: str) -> bytes:
     return sigdata
 
 
-def detect_crop(file) -> str:
+def signature_generator() -> Generator:
+    """Streaming signature generator (use gen.send(chunk)."""
+
+    def raw_generator():
+        sigfile = token_hex(16) + ".bin"
+        log.info(sigfile)
+        vf = "signature=format=binary:filename={}".format(sigfile)
+        cmd = [FFMPEG, "-i", "-", "-vf", vf, "-f", "null", "-"]
+        proc = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL, stdin=PIPE)
+        data = yield
+        while data:
+            proc.stdin.write(data)
+            data = yield
+        proc.stdin.close()
+        proc.wait()
+        with open(sigfile, "rb") as infile:
+            sigdata = infile.read()
+        os.remove(sigfile)
+        yield sigdata
+
+    initialized_generator = raw_generator()
+    next(initialized_generator)
+    return initialized_generator
+
+
+def detect_crop(file_path: str) -> str:
     """
     Detect crop value for video.
     Example result: crop=176:96:0:24
     """
-    cmd = [FFMPEG, "-i", file, "-vf", "cropdetect", "-f", "null", "-"]
+    cmd = [FFMPEG, "-i", file_path, "-vf", "cropdetect", "-f", "null", "-"]
     res = subprocess.run(cmd, stderr=subprocess.PIPE)
     text = res.stderr.decode(encoding=sys.stdout.encoding)
     crops = [
--- tests/test_video.py
+++ tests/test_video.py
@@ -14,9 +14,27 @@ def test_detect_crop():
     assert video.detect_crop(SAMPLE) == "crop=176:96:0:24"
 
 
-def test_compute_signature():
+def test_compute_signature_without_crop():
     sigh = blake3(video.compute_signature(SAMPLE)).hexdigest()
+    assert sigh == "021f4901f79bbb5c49edb0027103ec352f2bdb4feca53e6ce4f2f7d76c3dab5f"
+
+
+def test_compute_signature_with_crop():
+    crop = video.detect_crop(SAMPLE)
+    sigh = blake3(video.compute_signature(SAMPLE, crop)).hexdigest()
     assert sigh == "5dc1f30d13d798b062c2a47870364a9f5a6e2161bdd6e242e93eb5e6309bc4fa"
+
+
+def test_signature_generator():
+    gen = video.signature_generator()
+    with open(SAMPLE, 'rb') as infile:
+        data = infile.read(4096)
+        while data:
+            gen.send(data)
+            data = infile.read(4096)
+    result = next(gen)
+    sigh = blake3(result).hexdigest()
+    assert sigh == "021f4901f79bbb5c49edb0027103ec352f2bdb4feca53e6ce4f2f7d76c3dab5f"
 
 
 def test_content_id_video():
