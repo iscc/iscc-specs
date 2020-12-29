cf4b26eac3cabc4a8f9643690f72aa4f7a2eb6fa
Titusz Pan <titusz.pan@gmail.com>
2020-12-29 10:37:55 +0100

    Add Streamable context manager.

--- iscc/utils.py
+++ iscc/utils.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
+import io
 import os
+from typing import BinaryIO, Union
+
+
+File = Union[str, bytes, BinaryIO]
+
+
+class Streamable:
+    """Converts a file path or raw bytes into a managed readable stream."""
+
+    def __init__(self, data: File):
+        self.manage = True
+        if isinstance(data, str):
+            self.stream = open(data, "rb")
+        elif not hasattr(data, "read"):
+            self.stream = io.BytesIO(data)
+        else:
+            self.stream = data
+            self.manage = False
+
+    def __enter__(self):
+        return self.stream
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stream.close()
 
 
 class cd:
