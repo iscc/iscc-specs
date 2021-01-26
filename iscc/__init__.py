6018cc478175983cb21a5e0ef0d06076371adb0c
Titusz Pan <titusz.pan@gmail.com>
2021-01-26 15:16:54 +0100

    Tweak tika settings.

--- iscc/__init__.py
+++ iscc/__init__.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
+import os
+import appdirs
+
 __version__ = "1.0.5"
+APP_NAME = "iscc"
+APP_DIR = appdirs.user_data_dir(APP_NAME, roaming=False)
+os.makedirs(APP_DIR, exist_ok=True)
+os.environ["TIKA_PATH"] = APP_DIR
+os.environ["TIKA_LOG_PATH"] = APP_DIR
+os.environ["TIKA_STARTUP_MAX_RETRY"] = "8"
+os.environ["LOGURU_AUTOINIT"] = "False"
+from tika import tika
+
+tika.log.disabled = True
+
 from iscc.core import *
 from iscc.params import *
 from iscc.cdc import *
