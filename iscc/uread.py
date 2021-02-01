b1f1421f18b1e90fe3814b3352011e76d308990e
Titusz Pan <titusz.pan@gmail.com>
2021-02-01 23:05:28 +0100

    Simplify universal data reading.
    Former-commit-id: 508ce4401c13b27a3f6cfdbee597fe5ebcdcdbfc

--- iscc/uread.py
+++ iscc/uread.py
@@ -1,94 +1,19 @@
 # -*- coding: utf-8 -*-
 import io
-import os
-from os.path import basename, exists
-from pathlib import Path
-import mmap
-from typing import Union, BinaryIO, Optional
-from iscc.mediatype import guess
+from typing import Union
+from iscc.schema import Readable, Uri, File, Data
+from typing import BinaryIO
 
 
-Readable = Union[str, Path, bytes, bytearray, memoryview, BinaryIO]
-
-
-class uread:
-    """
-    Universal Reader.
-
-    Can handle differnt inputs such as file paths or file-like objects.
-    When used as a context manager it will only close the file if it was opened by
-    uread else it will restore the file cursor after exit.
-    """
-
-    def __init__(self, uri, filename=None):
-        # type: (Readable, Optional[str]) -> None
-        self._uri = uri
-        self._filename = filename
-        self._size = None
-        self._file = None
-        self._data = None
-        self._mediatype = None
-        self._start_pos = None
-        self._do_close = False
-
-        if hasattr(uri, "tell"):
-            # Store eventual cursor to restore at exit.
-            self._start_pos = uri.tell()
-
-        if isinstance(uri, (str, Path)):
-            if exists(uri):
-                self._filename = self._filename or basename(uri)
-                file = open(uri, mode="rb")
-                self._do_close = True  # We manage lifecyle
-                self._file = mmap.mmap(file.fileno(), length=0, access=mmap.ACCESS_READ)
-        elif isinstance(uri, (bytes, bytearray, memoryview)):
-            self._data = memoryview(uri)
-            self._size = len(uri)
-            self._file = io.BytesIO(self._data)
-            self._filename = self._filename or "undefined.und"
-        elif hasattr(uri, "read"):
-            self._file = uri
-        else:
-            raise ValueError(f"Cannot open {uri}")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self._start_pos is not None:
-            self.seek(self._start_pos)
-        if self._do_close:
-            self.close()
-
-    def tell(self):
-        return self._file.tell()
-
-    def seek(self, pos, whence=0):
-        return self._file.seek(pos, whence)
-
-    def read(self, num):
-        return self._file.read(num)
-
-    def size(self):
-        if self._size:
-            return self._size
-        elif hasattr(self._file, "size"):
-            return self._file.size()
-        elif hasattr(self._file, "fileno"):
-            return os.fstat(self._file.fileno()).st_size
-
-    @property
-    def filename(self):
-        if self._filename:
-            return self._filename
-        elif hasattr(self._file, "name"):
-            return basename(self._file.name)
-        elif hasattr(self._file, "filename"):
-            return basename(self._file.filename)
-
-    @property
-    def mediatype(self):
-        return self._mediatype or guess(self._uri)
-
-    def close(self):
-        return self._file.close()
+def open_data(data):
+    # type: (Readable) -> Union[BinaryIO]
+    """Open filepath, rawdata or file-like object."""
+    if isinstance(data, Uri.__args__):
+        return open(str(data), "rb")
+    elif isinstance(data, Data.__args__):
+        return io.BytesIO(data)
+    elif isinstance(data, File.__args__):
+        data.seek(0)
+        return data
+    else:
+        raise ValueError(f"Unupported data type {type(data)}")
