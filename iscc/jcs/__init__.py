8257a920480223aa91000949584603f7f6e9154e
Titusz Pan <titusz.pan@gmail.com>
2021-02-07 01:01:51 +0100

    Add rfc8785 JCS JSON canonicalization
    Former-commit-id: f2f6bb35d7ed5f75db85edaabdf3dcc468203cd2

---
+++ iscc/jcs/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from iscc.jcs.canonicalize import canonicalize
---
+++ tests/test_jsc.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import json
+from iscc import jcs
+import iscc
+
+
+def test_jcs_roundtrip():
+    data = dict(
+        text="  Iñtërnâtiôn\nàlizætiøn☃💩 –  is a tric\t ky \u00A0 thing!\r",
+        number=361665416,
+        fraction=65654.57354,
+    )
+    canonical = jcs.canonicalize(data)
+    roundtripped = json.loads(canonical)
+    assert data == roundtripped
+
+
+def test_jsc_text_normalized():
+    data = dict(
+        text="Hello World!",
+        number=361665416,
+        fraction=65654.57354,
+    )
+    canonical = jcs.canonicalize(data)
+    assert (
+        canonical.decode("utf-8")
+        == '{"fraction":65654.57354,"number":361665416,"text":"Hello World!"}'
+    )
+
+    assert (
+        iscc.normalize_text(canonical)
+        == '"fraction":65654.57354,"number":361665416,"text":"Hello World!"'
+    )
