aed0dc361146fe76b6129df3a223ed1acef3310a
Titusz Pan <titusz.pan@gmail.com>
2021-02-10 15:13:05 +0100

    Manage version pyproject.toml only.

--- iscc/__init__.py
+++ iscc/__init__.py
@@ -3,18 +3,21 @@ import os
 from pathlib import Path
 import click
 
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
 HERE = Path(__file__).parent
-# TIKA_BIN = HERE / "tika-server-1.25.jar"
-__version__ = "1.1.0-alpha.1"
 APP_NAME = "iscc"
 APP_DIR = click.get_app_dir(APP_NAME, roaming=False)
 os.makedirs(APP_DIR, exist_ok=True)
-
-# os.environ["TIKA_SERVER_JAR"] = TIKA_BIN.as_uri()
 os.environ["TIKA_STARTUP_MAX_RETRY"] = "20"
 os.environ["LOGURU_AUTOINIT"] = "False"
-from tika import tika
 
+from tika import tika
 tika.log.disabled = True
 
 from iscc.core import *
