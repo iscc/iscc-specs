dd22cc31125f6c8547c6667fd7672a9bc4d3ae56
Titusz Pan <titusz.pan@gmail.com>
2021-11-17 15:20:11 +0100

    Remove obsolete function from image module and make opencv import optional

--- iscc/image.py
+++ iscc/image.py
@@ -7,8 +7,6 @@ import pyexiv2
 from loguru import logger
 from pathlib import Path
 from typing import Any, List, Optional, Union
-import math
-from statistics import median
 from PIL import Image, ImageChops, ImageEnhance
 import numpy as np
 from more_itertools import chunked
@@ -17,7 +15,6 @@ from iscc.schema import FeatureType, Options, Readable
 from iscc.simhash import similarity_hash
 from iscc.text import normalize_text
 from iscc import uread
-import cv2 as cv
 
 
 IMAGE_META_MAP = {
@@ -98,6 +95,12 @@ def extract_image_features(data, n=32):
         sizes - keypoint sizes as percentage relative to the larger side of the image
         positions - percentage based x, y coordinates of the corresponding keypoints
     """
+    try:
+        import cv2 as cv
+    except ImportError as e:
+        raise EnvironmentError(
+            "Please install opencv-contrib-python-headless for granular image support"
+        )
 
     file = uread.open_data(data)
     img_array = np.array(bytearray(file.read()), dtype=np.uint8)
@@ -184,100 +187,3 @@ def trim_image(img):
         logger.debug(f"Image has been trimmed: {img}")
         return img.crop(bbox)
     return img
-
-
-def hash_image(img):
-    # type: (Union[Readable, Image.Image]) -> str
-    """Create hex coded image hash."""
-    pixels = normalize_image(img)
-    return hash_image_pixels(pixels).hex()
-
-
-def normalize_image(img):
-    # type: (Union[str, Path, Image.Image]) -> List[List[int]]
-    """Takes a path or PIL Image Object and returns a normalized array of pixels."""
-
-    if not isinstance(img, Image.Image):
-        img = Image.open(io.BytesIO(uread.open_data(img).read()))
-
-    if img.mode == "RGBA":
-        # Add white background to images that have an alpha transparency channel
-        bg = Image.new("RGB", img.size, (255, 255, 255))
-        bg.paste(img, mask=img.split()[3])  # 3 is alpha channel
-        img = bg
-
-    # 1. Convert to greyscale
-    img = img.convert("L")
-
-    # 2. Resize to 32x32
-    img = img.resize((32, 32), Image.BICUBIC)
-
-    # 3. Create two dimensional array
-    pixels = np.array(img).tolist()
-
-    return pixels
-
-
-def hash_image_pixels(pixels):
-    # type: (List[List[int]]) -> bytes
-    """Calculate image hash from 2D-Array of normalized image."""
-
-    # 1. DCT per row
-    dct_row_lists = []
-    for pixel_list in pixels:
-        dct_row_lists.append(compute_image_dct(pixel_list))
-
-    # 2. DCT per col
-    dct_row_lists_t = list(map(list, zip(*dct_row_lists)))
-    dct_col_lists_t = []
-    for dct_list in dct_row_lists_t:
-        dct_col_lists_t.append(compute_image_dct(dct_list))
-
-    dct_lists = list(map(list, zip(*dct_col_lists_t)))
-
-    # 3. Extract upper left 8x8 corner
-    flat_list = [x for sublist in dct_lists[:8] for x in sublist[:8]]
-
-    # 4. Calculate median
-    med = median(flat_list)
-
-    # 5. Create 64-bit digest by comparing to median
-    bitstring = ""
-    for value in flat_list:
-        if value > med:
-            bitstring += "1"
-        else:
-            bitstring += "0"
-    hash_digest = int(bitstring, 2).to_bytes(8, "big", signed=False)
-
-    return hash_digest
-
-
-def compute_image_dct(values_list):
-    """
-    Discrete cosine transform algorithm by Project Nayuki. (MIT License)
-    See: https://www.nayuki.io/page/fast-discrete-cosine-transform-algorithms
-    """
-
-    n = len(values_list)
-    if n == 1:
-        return list(values_list)
-    elif n == 0 or n % 2 != 0:
-        raise ValueError()
-    else:
-        half = n // 2
-        alpha = [(values_list[i] + values_list[-(i + 1)]) for i in range(half)]
-        beta = [
-            (values_list[i] - values_list[-(i + 1)])
-            / (math.cos((i + 0.5) * math.pi / n) * 2.0)
-            for i in range(half)
-        ]
-        alpha = compute_image_dct(alpha)
-        beta = compute_image_dct(beta)
-        result = []
-        for i in range(half - 1):
-            result.append(alpha[i])
-            result.append(beta[i] + beta[i + 1])
-        result.append(alpha[-1])
-        result.append(beta[-1])
-        return result
